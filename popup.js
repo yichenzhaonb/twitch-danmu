eac568b6e0dab46758a765f90d4d3b5b0dd3ad16
yichen.zhao <yichen.zhao@americaneagle.com>
2021-01-25 23:39:25 -0600

    add changes

--- popup.js
+++ popup.js
@@ -4,15 +4,18 @@
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
+// let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+// chrome.storage.sync.get('color', function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute('value', data.color);
+// });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-    chrome.tabs.executeScript(
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-};
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//     chrome.tabs.executeScript(
+//         {code: 'document.body.style.backgroundColor = "' + color + '";'});
+// };
+
+// let danmu = document.getElementsByClassName('text-fragment');
+// console.log(danmu);
