073e66ec33652a4138e52d2c22b8b4c95cc55079
yichen.zhao <yichenzhao18@gmail.com>
2021-02-14 12:57:32 -0600

    add change

--- danmu.js
+++ danmu.js
@@ -1,32 +1,39 @@
+var currentDanmu= "";
+var i = 0;
 
-   
+// let elem = document.createElement('div');
+// elem.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000';
+// document.getElementsByClassName("video-player__overlay").appendChild(elem);
 //turn on danmu
 var addListeners=function(){
     let danmu = document.getElementsByClassName("text-fragment");
-    let danmuArr = [];
+    let twitch = document.getElementsByClassName("video-player__overlay");
+console.log( twitch);
+console.log( document.getElementsByClassName("video-player__overlay"));
    // for (i=0; i<danmu.length;i++){    
-    danmuArr.push(danmu[0].textContent);  
+    //danmuArr.push(danmu[0].textContent,new Date().getTime());  
     //}
-    console.log(danmuArr);
-
-   // console.log("Danmu on");
+    document.getElementsByClassName("video-player__overlay").style.backgroundColor = "lightblue";
+    if(danmu[i]){
+        currentDanmu = danmu[i].textContent;
+        console.log(currentDanmu);
+        twitch.innerHTML += '<div class="danmu-overlay">'+ currentDanmu +'</div>';
+        i++;
+    }
+ 
 }
 
 //turn off danmu
 var removeListeners = function(){
-
      console.log("Danmu off");
 }
-
 //message listener for background
-
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)    {
     if(request.command === 'init'){
-        setInterval(() => {
-        addListeners();
-    }, 5000); 
-
+    //     setInterval(() => {
+    //     addListeners();
+    // }, 500); 
+    addListeners();
     }else{
         removeListeners();
     }
