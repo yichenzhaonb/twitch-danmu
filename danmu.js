5ba31069dcd85239f7e162496439be6f6fc16645
yichen.zhao <yichen.zhao@americaneagle.com>
2021-02-14 01:56:03 -0600

    add change

--- danmu.js
+++ danmu.js
@@ -1,9 +1,15 @@
 
-
+   
 //turn on danmu
 var addListeners=function(){
+    let danmu = document.getElementsByClassName("text-fragment");
+    let danmuArr = [];
+   // for (i=0; i<danmu.length;i++){    
+    danmuArr.push(danmu[0].textContent);  
+    //}
+    console.log(danmuArr);
 
-    console.log("Danmu on");
+   // console.log("Danmu on");
 }
 
 //turn off danmu
@@ -13,12 +19,16 @@ var removeListeners = function(){
 }
 
 //message listener for background
+
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)    {
     if(request.command === 'init'){
+        setInterval(() => {
         addListeners();
+    }, 5000); 
+
     }else{
         removeListeners();
     }
     sendResponse({result: "success"});
 });
-
--- popup.js
+++ popup.js
@@ -27,7 +27,7 @@
  danmuOption.onchange = function(e){
    let value = this.checked;
 
-   chrome.storage.sync.set({'hide': value}, function() {
+  chrome.storage.sync.set({'hide': value}, function() {
     console.log('The value is'+ value);
   });
 
@@ -42,7 +42,7 @@
    }else{
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {command: "remove", hide: value}, function(response) {
-                     // console.log(response.result);
+                    // console.log(response.result);
         });
       });
 
